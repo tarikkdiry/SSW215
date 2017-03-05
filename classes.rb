98f0cde897a966f93eb3933e037e65c636f9b979
Tarik Kdiry <tarik.kdiry@gmail.com>
2017-03-05 12:11:09 -0500

    .

--- classes.rb
+++ classes.rb
@@ -4,29 +4,59 @@
 
 
 h = {}
+h['class'] = Array.new
+h['class'] << 215
+h['class'] << 315
+h['class'] << 322
+h['class'] << 345
+h['class'] << 555
 
+# class[215] = {}
 begin
-  puts "Give me the name of the student and the classes they are enrolled in."
-  puts "When finished, enter 0"
+  puts "Give me your student, then their three classes."
+  answer = gets.split.to_s
 
-  name = gets.to_s
+  
 
-  check = name.to_i
+end
 
-  if check == 0
-    classes = [gets.to_i]
-    n = classes.length
-    n.times do
-      if h.key?(classes[n])
-        temp = h[classes[n]] + name
-        n -= 1
-      end
-      if not h.key?(classes[n])
-        h[classes[n] => name]
-        n -= 1
-      end
-    end
-  end
-end until check == 1
 
-puts h
+
+
+
+
+# class2 = gets.to_i
+# class3 = gets.to_i
+
+# if h['class'].include?(class1)
+#   puts class1.to_s + ":"
+#   puts student_name
+# end
+
+
+
+# begin
+#   puts "Give me the name of the student and the classes they are enrolled in."
+#   puts "When finished, enter 0"
+#
+#   name = gets.to_s
+#
+#   check = name.to_i
+#
+#   if check == 0
+#     classes = [gets.to_i]
+#     n = classes.length
+#     n.times do
+#       if h.key?(classes[n])
+#         temp = h[classes[n]] + name
+#         n -= 1
+#       end
+#       if not h.key?(classes[n])
+#         h[classes[n] => name]
+#         n -= 1
+#       end
+#     end
+#   end
+# end until check == 1
+#
+# puts h
